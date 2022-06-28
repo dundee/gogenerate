c62b0df23a584e6e5d24dbb1ef7597cc2aa96e39
Daniel Milde <daniel@milde.cz>
2022-06-28 13:19:04 +0200

    custom option

--- protobuf/generate.go
+++ protobuf/generate.go
@@ -1,3 +1,3 @@
 package protobuf
 
-//go:generate protoc --go_out=. --go_opt=paths=source_relative ./user.proto
+//go:generate protoc --go_out=. --go_opt=paths=source_relative ./user.proto ./log.proto
